585e267a0ab507c06ff15fe05033331984e8f43e
ArthurKorablev <arttr86@gmail.com>
2023-01-06 21:02:25 +0200

    code for hw16

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const btnSubmit = document.querySelector('#btnSubmit');
 const form = document.querySelector('.page');
 
 const inputName = document.querySelector('#floatingInputValue');
+const email = document.querySelector('#floatingInput');
+const phone = document.querySelector('#floatingInputPhone');
 const city = document.querySelector('select');
 const radios = document.querySelectorAll('input[name="flexRadioDefault"]');
 const quantity = document.querySelector('#formGroupExampleInput');
@@ -12,7 +14,26 @@ function validation() {
 
     let flag = true;
     let formInputs = [];
-    formInputs.push(inputName, city, quantity);
+    let regName = /^[a-zA-Z]{2,} [a-zA-Z]+ [a-zA-Z]+$/g
+    let regEmail = /^((([0-9A-Za-z]{1}[-0-9A-z\.]{1,}[0-9A-Za-z]{1}))@([-A-Za-z]{1,}\.){1,2}[-A-Za-z]{2,})$/g
+    let regPhone = /(\s*)?(\+)?([- _():=+]?\d[- _():=+]?){10,14}(\s*)?/g
+    formInputs.push(city, quantity);
+
+    inputName.classList.remove('is-invalid');
+    if (!regName.test(inputName.value)) {
+        flag = false;
+        inputName.classList.add('is-invalid');
+    }
+    email.classList.remove('is-invalid');
+    if (!regEmail.test(email.value)) {
+        flag = false;
+        email.classList.add('is-invalid');
+    }
+    phone.classList.remove('is-invalid');
+    if (!regPhone.test(phone.value)) {
+        flag = false;
+        phone.classList.add('is-invalid');
+    }
 
     formInputs.forEach(input => {
         input.classList.remove('is-invalid');
@@ -38,16 +59,22 @@ function output() {
     const name = document.createElement('h2');
     const location = document.createElement('p');
     const quantityOfProduct = document.createElement('p');
+    const phoneDiv = document.createElement('p');
+    const emailDiv = document.createElement('p');
 
     name.textContent = `${inputName.value}`;
     location.textContent = `City: ${city.value}`;
     quantityOfProduct.textContent = `Quantity: ${quantity.value}`;
+    phoneDiv.textContent = `Phone: ${phone.value}`;
+    emailDiv.textContent = `Email: ${email.value}`;
 
     name.classList.add('output');
     location.classList.add('output');
     quantityOfProduct.classList.add('output');
+    phoneDiv.classList.add('output');
+    emailDiv.classList.add('output');
 
-    mainDiv.append(name, location, quantityOfProduct);
+    mainDiv.append(name, phoneDiv, emailDiv, location, quantityOfProduct);
 
     if (textarea.value != "") {
         const coment = document.createElement('p');
